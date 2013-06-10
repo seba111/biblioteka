acfa4d3fcc025945c8b6d5407af729489ade1de0
Bartosz Kiebdaj <bartoszkiebdaj@gmail.com>
2013-06-10 19:48:11 +0200

    js

--- Biblioteka-war/web/js/document.js
+++ Biblioteka-war/web/js/document.js
@@ -14,6 +14,42 @@ $(document).ready(function(){
        $('.about-history').hide();
        setActiveClass($(this),'.nav li');
    });
+   
+   
+   (function(){
+       
+       switch(window.action){
+           case "/glowna.xhtml":
+           case "/wydarzenia.xhtml":    
+               $('.active-glowna').addClass('active');
+               break;
+           case "/about.xhtml":
+               
+               $('.active-about').addClass('active');
+               break;
+           case "/catalog.xhtml":
+           case "/searchresult.xhtml":    
+               $('.active-catalog').addClass('active');
+               break;
+               
+           case "/editUsers.xhtml":
+               $('.active-users').addClass('active');
+               break;
+           case "/addNews.xhtml":
+               $('.active-news').addClass('active');
+               break ;
+           case "/addBook.xhtml":
+               $('.active-addbook').addClass('active');
+               break ;   
+           case "/profile.xhtml":
+               $('.active-profile').addClass('active');
+               break ;   
+           default :
+               $('.active-glowna').addClass('active');
+               break;
+       }
+   })();
+   
 });
 
 
